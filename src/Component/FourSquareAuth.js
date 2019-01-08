e1998166ced0a19da2e226c3cea98c6d68eebde0
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-01-08 15:16:02 -0500

    Foursquare oauth token returned as hash value.

--- src/Component/FourSquareAuth.js
+++ src/Component/FourSquareAuth.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 export default class FourSquareAuth extends React.Component {
   
@@ -7,8 +8,23 @@ export default class FourSquareAuth extends React.Component {
     const FOURSQUARE_CLIENT_ID = 'SOTVCGCEZKOO3WRVWQ0Q4TWAFT2JCHFO4CAAATWEVXPS2HTY'; // this one is for local dev. http://127.0.0.1:3000
     const FOURSQUARE_REDIRECT_URI = `http://${window.location.href.split("/")[2]}/error`;
 
-    const url = `https://foursquare.com/oauth2/authenticate?client_id=${FOURSQUARE_CLIENT_ID}&response_type=code&redirect_uri=${FOURSQUARE_REDIRECT_URI}`;
+    const url = `https://foursquare.com/oauth2/authenticate?client_id=${FOURSQUARE_CLIENT_ID}&response_type=token&redirect_uri=${FOURSQUARE_REDIRECT_URI}`;
     window.location.href = url
+
+  //   const FOURSQUARE_CLIENT_ID = 'SOTVCGCEZKOO3WRVWQ0Q4TWAFT2JCHFO4CAAATWEVXPS2HTY'; // this one is for local dev. http://127.0.0.1:3000
+  //   const FOURSQUARE_SECRET_ID = 'DUGP4HATBM5TJCY3CJRXEBCWG2MM1BM0GIHEM5HZDUMFIBL1';
+  //   const FOURSQUARE_REDIRECT_URI = `http://${window.location.href.split("/")[2]}/error`;
+    
+  //   const meetupConfig = {
+  //     url: `https://foursquare.com/oauth2/access_token?client_id=${FOURSQUARE_CLIENT_ID}&client_secret=${FOURSQUARE_SECRET_ID}&response_type="authorization_code"&redirect_uri=${FOURSQUARE_REDIRECT_URI}&code=ZVUGMTCF2EWF3ZGEC4Z3FL031JDFCSC5KOUWYL2OESJ2R4FZ#_=_`,
+  //     headers: {
+  //       "Accept": "*/*"
+  //     }
+  // }
+  //   axios(meetupConfig)
+  //   .then(res => { console.log(res); })
+  //   .catch(err => { console.log(err); })
+
 
     return <div>
       {/**/}
